e1266e1d25c6f128b57fcdf55090e879739dd1b2
githubutilities <githubutilities@163.com>
2016-08-30 22:29:45 +0800

    Remove cask install line.
    Cask is homebrew's builtin now.

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,8 +7,5 @@
 # install homebrew
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-# install homebrew-cask
-brew install caskroom/cask/brew-cask
-
 # install my dotfiles
 cd "${HOME}" && git clone https://github.com/githubutilities/dotfiles.git && cd dotfiles && git checkout osx && source bootstrap.sh
